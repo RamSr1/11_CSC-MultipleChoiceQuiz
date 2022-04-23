cc531532a75f898bba9aca179f2452e35582eaab
Sri Ram Ram <102088352+RamSr1@users.noreply.github.com>
2022-04-23 08:22:06 +0000

    Added A Class For Timer

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 import threading
 import time
 import sys
+
+class SecondCounter(threading.Thread):
+    '''
+    create a thread object that will do the counting in the background
+    default interval is 1/1 of a second
+    '''
