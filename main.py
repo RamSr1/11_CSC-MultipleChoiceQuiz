8363bc9e5c22f47ec9fd75f52bcfa9b532a30435
Sri Ram Ram <102088352+RamSr1@users.noreply.github.com>
2022-04-23 10:01:08 +0000

    Timer Starter

--- main.py
+++ main.py
@@ -8,37 +8,37 @@ class SecondCounter(threading.Thread):
     default interval is 1/1 of a second
     '''
     def __init__(self, interval=1):
-          # init the thread
-          threading.Thread.__init__(self)
-          self.interval = interval  # seconds
-          # initial value
-          self.value = 0
-          # controls the while loop in method run
-          self.alive = False
-  
-      def run(self):
-          '''
-          this will run in its own thread via self.start()
-          '''
-          self.alive = True
-          while self.alive:
-              time.sleep(self.interval)
-              # update count value
-              self.value += self.interval
-  
-      def peek(self):
-          '''
-          return the current value
-          '''
-          return self.value
-  
-      def finish(self):
-          '''
-          close the thread, return final value
-          '''
-          # stop the while loop in method run
-          self.alive = False
-          return self.value
+        # init the thread
+        threading.Thread.__init__(self)
+        self.interval = interval  # seconds
+        # initial value
+        self.value = 0
+        # controls the while loop in method run
+        self.alive = False
+
+    def run(self):
+        '''
+        this will run in its own thread via self.start()
+        '''
+        self.alive = True
+        while self.alive:
+            time.sleep(self.interval)
+            # update count value
+            self.value += self.interval
+
+    def peek(self):
+        '''
+        return the current value
+        '''
+        return self.value
+
+    def finish(self):
+        '''
+        close the thread, return final value
+        '''
+        # stop the while loop in method run
+        self.alive = False
+        return self.value
 
 
 def intro():
@@ -54,3 +54,11 @@ def intro():
            break
        else:
            print("  Please enter your first name with letters only, and don't leave empty spaces")
+           
+  
+  
+  inst=input("  Press enter to proceed  :")
+  print("")
+  print("╚════════════════════════════════════════╝")
+  
+ 
